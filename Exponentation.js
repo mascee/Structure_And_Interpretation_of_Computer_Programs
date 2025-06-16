9ef0ee9cd7034789c03e96d6e4b72b897bc81429
Olga Petrova <mascee@gmail.com>
2025-06-16 15:11:27 -0700

    Added Exponentations.js

--- Exponentation.js
+++ Exponentation.js
@@ -20,10 +20,51 @@ function expt_iter(b, counter, product){
     return counter == 0 ? product : expt_iter(b, counter - 1, b * product);
 }
 
-
 console.log('Linear iteration, Exponents of 3: ')
 console.log(expt(3, 0))
 console.log(expt(3, 1))
 console.log(expt(3, 2))
 console.log(expt(3, 3))
 console.log(expt(3, 4))
+
+// Optimizing for odd and even numbers
+function fast_expt(b, n){
+    return n == 0 ? 1 : is_even(n) ? square(fast_expt(b, n/2)):b * fast_expt(b, n-1);
+}
+function is_even(n){
+    return n % 2 == 0;
+}
+function square(x){
+    return x * x;
+}
+
+console.log('Testing odd and even numbers: ')
+console.log(fast_expt(3, 0))
+console.log(fast_expt(3, 1))
+console.log(fast_expt(3, 2))
+console.log(fast_expt(3, 3))
+console.log(fast_expt(3, 4))
+
+console.log(fast_expt(2, 0))
+console.log(fast_expt(2, 1))
+console.log(fast_expt(2, 2))
+console.log(fast_expt(2, 3))
+console.log(fast_expt(2, 4))
+
+// Adding negative exponents
+function all_fast_expt(b,n){
+    if (n=== 0) return 1;
+    if (n < 0) return 1 / all_fast_expt(b, -n);
+    if (n % 2 === 0){
+        let half = all_fast_expt(b, n/2);
+        return half * half;
+    }
+    else{
+        return b * all_fast_expt(b, n-1);
+    }
+}
+
+console.log('Testing negaive exponents')
+console.log(all_fast_expt(-2, 2))
+console.log(all_fast_expt(2, -2))
+console.log(all_fast_expt(0.5, 2))
